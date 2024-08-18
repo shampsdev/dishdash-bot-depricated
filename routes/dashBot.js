71f2279531b29418e606ce7cd0182555aa4f651e
AlexDyakonov <shram.monolit@mail.ru>
2024-08-18 11:02:51 +0300

    Optimised code

--- routes/dashBot.js
+++ routes/dashBot.js
@@ -28,6 +28,13 @@ router.use(
 const { devlog, handleError } = require('./common.js');
 
 const { sendMessage, getUserProfilePicture } = require('./methods.js');
+const {
+  handleReportMessage,
+  handleStartCommand,
+  handleHelpCommand,
+  handleReportCommand,
+  handleUnknownCommand,
+} = require('./commands.js'); 
 
 setTimeout(function () {
   axios
@@ -85,73 +92,20 @@ router.post(`/hook`, (req, res) => {
       if (currentSession.reportMode) {
         currentSession.reportMode = false;
 
-        if (report_chat_id === undefined) {
+        if (reportChatId === undefined) {
           console.log("`report_chat_id` is undefined. Check envs to turn on reports.");
         } else {
-          let reportTime = new Date().toLocaleString();
-
-          sendMessage(
-            {
-              chat_id: reportChatId,
-              text: `⚠️ <b>Репорт от пользователя:</b>\n\n<b>Имя:</b> ${message.from.first_name} ${message.from.last_name || ''}\n<b>Юзернейм:</b> @${message.from.username || 'нет юзернейма'}\n<b>Chat ID:</b> ${chat_id}\n<b>Время обращения:</b> ${reportTime}\n\n<b>Сообщение:</b>\n${text}`,
-              parse_mode: 'HTML',
-            },
-            'sendMessage',
-            token
-          );
-
-          sendMessage(
-            {
-              chat_id: chat_id,
-              text: 'Ваш репорт был успешно отправлен администрации.',
-            },
-            'sendMessage',
-            token
-          );
+          handleReportMessage(message, chat_id, reportChatId, token);
         }
       } else if (text === '/start') {
-        let fullName = message.from.first_name;
-        if (message.from.last_name) {
-          fullName += ` ${message.from.last_name}`;
-        }
-  
-        sendMessage(
-          {
-            chat_id: chat_id,
-            text: `Добро пожаловать, <b>${fullName}</b>!\n\nИспользуйте DishDash, чтобы легко и быстро выбрать место для встречи в компании. Упомяните бота в чате и введите <code>@dishdash_bot start</code> для создания лобби.\n\nБот <a href='https://dishdash.ru'>DishDash</a> разработан командой <a href='https://t.me/+4l8DChDSxMQxNWUy'>\"Шампиньоны\"</a>.`,
-            parse_mode: 'HTML',
-          },
-          'sendMessage',
-          token
-        );
+        handleStartCommand(chat_id, message, token);
       } else if (text === '/help') {
-        sendMessage(
-          {
-            chat_id: chat_id,
-            text: 'Доступные команды:\n/start - Запуск бота\n/help - Справка\n/report - сообщить о баге/проблеме',
-          },
-          'sendMessage',
-          token
-        );
+        handleHelpCommand(chat_id, token);
       } else if (text === '/report') {
         currentSession.reportMode = true;
-        sendMessage(
-          {
-            chat_id: chat_id,
-            text: 'Теперь отправьте сообщение, которое вы хотите сообщить администрации.',
-          },
-          'sendMessage',
-          token
-        );
+        handleReportCommand(chat_id, token);
       } else {
-        sendMessage(
-          {
-            chat_id: chat_id,
-            text: 'Извините, я не понимаю эту команду.',
-          },
-          'sendMessage',
-          token
-        );
+        handleUnknownCommand(chat_id, token);
       }
     }
   }
